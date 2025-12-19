0758347e721120ef104251fb81a4304898264a04
kavan1408 <8a.18.kavanvanshikumar.chaudhari@gmail.com>
2025-12-19 19:50:17 +0530

    Added Python programs

---
+++ graph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3]
+y = [2, 4, 6]
+
+plt.plot(x, y)
+plt.show()
+
+import pandas as pd
+
+data = {'Name':['A','B'], 'Marks':[80,90]}
+df = pd.DataFrame(data)
+print(df)
